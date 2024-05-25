93747c05b1ee1b8f5c6b0e6cce37a285a9f85d86
Roqeaiyakhan <roqaiyajaani@gmail.com>
2024-05-25 21:14:37 +0300

    add chp 5,6,

---
+++ home.js
@@ -0,0 +1,21 @@
+//chpter 5
+//console.log(5 * 5 -3 / 3 + 7);
+//25- 3 / 3 + 7
+//25 - 1 + 7
+//24 + 7
+//31
+
+
+//var number = 2 + 2;
+//console.log ({number});
+
+//var firstNumber  = 18;
+//var secondNumber = 12;
+//var result = firstNumber - secondNumber;
+//console.log({result});
+
+//var num = 1;
+//var newNum = ++num;
+//console.log(newNum++);
+
+alert("Thanks, " + userName + "!");
