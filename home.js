4057b129a30a485f0ea4a97edc1e78efb48ec31b
Roqeaiyakhan <roqaiyajaani@gmail.com>
2024-05-28 12:23:55 +0500

    add

--- home.js
+++ home.js
@@ -1,11 +1,11 @@
-//chpter 5
+//chpter 7
 //console.log(5 * 5 -3 / 3 + 7);
 //25- 3 / 3 + 7
 //25 - 1 + 7
 //24 + 7
 //31
 
-
+//chp 5
 //var number = 2 + 2;
 //console.log ({number});
 
@@ -17,5 +17,27 @@
 //var num = 1;
 //var newNum = ++num;
 //console.log(newNum++);
+var num = 1;
+var newNum = num++;
+console.log(newNum);
 
-alert("Thanks, " + userName + "!");
+//subtraction
+//var x = 2 - "1";
+//console.log(x);
+
+//var x = "3" - 2 - "4";
+//console.log(x);
+
+//var x = "g" - 3;
+// console.log(x)
+
+//Multiplication
+//var x = 5 * "3";
+//console.log(x);
+
+//var firstNumber = "g" * 4;
+//console.log(firstNumber);
+
+//Division
+//var x = 7 / 5;
+//console.log(x);
